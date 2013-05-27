4c1c7a75619327f1cb1529a0fbfd31a397183aa5
Shinpei Maruyama <shinpeim@gmail.com>
2013-05-27 14:42:16 +0900

    突然死ナイザーを実装

--- src/main/scala/sudden_killer.scala
+++ src/main/scala/sudden_killer.scala
@@ -7,16 +7,37 @@ object SuddenKiller {
   val tokenizer = Tokenizer.builder().build()
 
   def main(args: Array[String]) : Unit = {
-    val words = parse(args.first)
-    words.foreach(w => println(w.surface + ":" + w.partOfSpeach))
+    val input:String = args.firstOption.getOrElse("")
+    val suddenized: String = suddenize(input).getOrElse(input)
+    println(suddenized)
   }
 
-  def parse(s: String) : List[Word] = {
+  def suddenize(s:String): Option[String] = {
+    val reversedWords: List[Word] = tokenize(s).reverse
+
+    val wordsToString: List[Word] => String = words => words.foldLeft(""){(a,b) => a + b.surface}
+    val addSuddenDeath: String => String = s => s + """
+＿人人人人人人＿
+＞　突然の死　＜
+￣ＹＹＹＹＹＹ￣
+"""
+
+    matchedReversedWords(reversedWords).map(_.reverse).map(wordsToString).map(addSuddenDeath)
+  }
+
+  def tokenize(s: String) : List[Word] = {
     def takePartOfString(allFeatures: String) = {
       allFeatures.split(',').first
     }
 
     val tokens:List[Token] = tokenizer.tokenize(s).toList
     tokens.map(t => Word(t.getSurfaceForm, takePartOfString(t.getAllFeatures)))
+  }
+
+  def matchedReversedWords(reversedWords: List[Word]): Option[List[Word]] = reversedWords match {
+    case Nil => None
+    case Word("の", "助詞") :: Word(_, "名詞") :: _ |
+         Word("が", "助詞") :: Word(_, "名詞") :: _ => Some(reversedWords)
+    case _ :: rest => matchedReversedWords(rest)
   }
 }
