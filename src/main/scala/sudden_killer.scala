9c3b1998638d68c1f637bb7c714d4289148c723b
Shinpei Maruyama <shinpeim@gmail.com>
2013-05-27 20:47:18 +0900

    Refactor

--- src/main/scala/sudden_killer.scala
+++ src/main/scala/sudden_killer.scala
@@ -1,8 +1,23 @@
 import com.atilika.kuromoji._
 import collection.JavaConversions._
 
-object SuddenKiller {
-  case class Word(surface: String, partOfSpeach: String)
+object Suddenizer {
+  class Word(surface: String) {
+    override val toString = surface
+  }
+  case class Noun     (surface: String) extends Word(surface)
+  case class Particle (surface: String) extends Word(surface)
+  case class OtherWord(surface: String) extends Word(surface)
+
+  object Word {
+    def fromToken(t: Token): Word = {
+      t.getAllFeatures.split(',').first match {
+        case "名詞" => Noun(t.getSurfaceForm)
+        case "助詞" => Particle(t.getSurfaceForm)
+        case _      => OtherWord(t.getSurfaceForm)
+      }
+    }
+  }
 
   lazy val tokenizer = Tokenizer.builder().build()
   val SUDDEN_DEATH_STRING = """
@@ -11,25 +26,27 @@ object SuddenKiller {
 ￣ＹＹＹＹＹＹ￣
 """
 
-  def main(args: Array[String]) = {
-    val input = args.firstOption.getOrElse("")
-    val suddenized = suddenize(input).getOrElse(input)
-    println(suddenized)
-  }
-
   def suddenize(s: String): Option[String] = {
-    val words = tokenize(s).map(tokenToWord)
-    val wordsToSuddenize = takeReversedWordsToSuddenize(words.reverse).map(_.reverse)
-    wordsToSuddenize.map(wordsToString).map(_ + SUDDEN_DEATH_STRING)
+    val words = tokenize(s).map(Word.fromToken)
+    takeReversedWordsToSuddenize(words.reverse).map(_.reverse) match {
+      case Some(words) => Some(words.map(_.toString).reduce(_ + _) + SUDDEN_DEATH_STRING)
+      case None => None
+    }
   }
 
   private def tokenize(s: String): List[Token] = tokenizer.tokenize(s).toList
-  private def tokenToWord(t: Token): Word = Word(t.getSurfaceForm, t.getAllFeatures.split(',').first)
   private def takeReversedWordsToSuddenize(reversedWords:List[Word]): Option[List[Word]] = reversedWords match {
     case Nil => None
-    case Word("の", "助詞") :: Word(_, "名詞") :: _ |
-         Word("が", "助詞") :: Word(_, "名詞") :: _ => Some(reversedWords)
+    case Particle("の") :: Noun(_) :: _ |
+         Particle("が") :: Noun(_) :: _ => Some(reversedWords)
     case _ :: rest => takeReversedWordsToSuddenize(rest)
   }
-  private def wordsToString(words:List[Word]) = words.foldLeft(""){(a,b) => a + b.surface}
+}
+
+object SuddenKiller {
+  def main(args: Array[String]) = {
+    val input = args.firstOption.getOrElse("")
+    val suddenized = Suddenizer.suddenize(input).getOrElse(input)
+    println(suddenized)
+  }
 }
