26be829dedf3a5b7de57f5382a55a71cc37d53c0
Shinpei Maruyama <shinpeim@gmail.com>
2013-05-27 11:19:25 +0900

    parse が List[Word] を返すように

--- src/main/scala/sudden_killer.scala
+++ src/main/scala/sudden_killer.scala
@@ -1,19 +1,22 @@
 import com.atilika.kuromoji._
 import collection.JavaConversions._
 
+case class Word(surface: String, partOfSpeach: String)
+
 object SuddenKiller {
+  val tokenizer = Tokenizer.builder().build()
 
-    val tokenizer = Tokenizer.builder().build()
-    def main(args: Array[String]) : Unit = {
-        args.firstOption match {
-            case None    => println("pleas pass a sentence")
-            case Some(s) => parse(s)
-        }
+  def main(args: Array[String]) : Unit = {
+    val words = parse(args.first)
+    words.foreach(w => println(w.surface + ":" + w.partOfSpeach))
+  }
+
+  def parse(s: String) : List[Word] = {
+    def takePartOfString(allFeatures: String) = {
+      allFeatures.split(',').first
     }
 
-    def parse(s: String) : Unit = {
-        val tokens:List[Token] = tokenizer.tokenize(s).toList
-        val words:List[String] = tokens.map(t => t.getSurfaceForm + ":" + t.getAllFeatures)
-        words.foreach(println)
-    }
+    val tokens:List[Token] = tokenizer.tokenize(s).toList
+    tokens.map(t => Word(t.getSurfaceForm, takePartOfString(t.getAllFeatures)))
+  }
 }
