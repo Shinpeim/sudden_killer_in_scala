72862cf36f643c1eeb029b9a3dd2cf3cce689cac
Shinpei Maruyama <shinpeim@gmail.com>
2013-05-29 18:08:41 +0900

    logger を仕込んだ

--- src/main/scala/twitter/stream_handler.scala
+++ src/main/scala/twitter/stream_handler.scala
@@ -3,20 +3,30 @@ package com.nekogata.SuddenKiller.twitter {
   import com.nekogata.SuddenKiller.Suddenizer
   import com.nekogata.SuddenKiller.twitter.{Configuration => SuddenKillerConfiguration}
   import scala.util.matching.Regex
+  import org.apache.logging.log4j.{Logger, LogManager}
 
   class StreamHandler(configuration: SuddenKillerConfiguration) extends UserStreamAdapter {
+    val log = LogManager.getLogger("StreamHandler")
     val twitter = (new TwitterFactory(configuration.getTwitterConfig)).getInstance
     val interval = 60 * 15 * 1000 //msec.
     var keepSilentUntil = System.currentTimeMillis + interval
 
-    override def onStatus(status: Status) = status.getText match {
-      case s if s.matches(".*@totsuzenshi_bot.*unfollow.*") => unfollow(status.getUser)
-      case s if s.matches(".*@.*") => Unit // ignore if text include reply to someone
-      case s if s.matches(".*RT.*") => Unit // ignore if retweeted tweet
-      case s => suddenize(s)
+    override def onStatus(status: Status) = {
+      val user = status.getUser
+      val text = status.getText
+
+      log.debug("recieve status: " + user.getScreenName + text)
+
+      text match {
+        case s if s.matches(".*@totsuzenshi_bot.*unfollow.*") => unfollow(status.getUser)
+        case s if s.matches(".*@.*") => Unit // ignore if text include reply to someone
+        case s if s.matches(".*RT.*") => Unit // ignore if retweeted tweet
+        case s => suddenize(s)
+      }
     }
 
     override def onFollow(source: User, followedUser: User) = {
+      log.debug("recieve follow event: " + source.getScreenName + " follows " + followedUser.getScreenName)
       if ( !source.isProtected && source.getScreenName != "totsuzenshi_bot") followBack(source)
     }
 
@@ -33,15 +43,17 @@ package com.nekogata.SuddenKiller.twitter {
     }
 
     private def followBack(user: User) = try {
+      log.info("followbacking...: " + user.getScreenName)
       twitter.createFriendship(user.getId)
+      log.info("followbacked: " + user.getScreenName)
     } catch {
-      case (e: Throwable) => println(e)
+      case (e: Throwable) => log.warn(e)
     }
 
     private def unfollow(user: User) = try {
       twitter.destroyFriendship(user.getId)
     } catch {
-      case (e: Throwable) => println(e)
+      case (e: Throwable) => log.warn(e)
     }
   }
 }
