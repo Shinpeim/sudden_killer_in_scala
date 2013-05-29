80326d859e17d4a1c80c7da22723d2c5e698fc8c
Shinpei Maruyama <shinpeim@gmail.com>
2013-05-29 20:47:15 +0900

    Refactor

--- src/main/scala/twitter/stream_handler.scala
+++ src/main/scala/twitter/stream_handler.scala
@@ -11,19 +11,22 @@ package com.nekogata.SuddenKiller.twitter {
     val interval = 60 * 15 * 1000 //msec.
     var keepSilentUntil = System.currentTimeMillis + interval
 
+    type StatusHandler = Function1[Status, Unit]
+    type OnStatusPattern = PartialFunction[Status, Option[StatusHandler]]
+    val onStatusPattern: OnStatusPattern = {
+      case s if s.getUser.isProtected => None
+      case s if s.isRetweet => None
+      case s if s.getText.matches(".*@.*") => None
+      case s if s.getText.matches(".*@totsuzenshi_bot.*unfollow.*") => Some(unfollow)
+      case s => Some(suddenize)
+    }
+
     override def onStatus(status: Status) = {
       val user = status.getUser
       val text = status.getText
-
       log.debug("recieve status: " + user.getScreenName + text)
 
-      status match {
-        case s if s.getUser.isProtected => Unit
-        case s if s.isRetweet => Unit
-        case s if s.getText.matches(".*@.*") => Unit
-        case s if s.getText.matches(".*@totsuzenshi_bot.*unfollow.*") => unfollow(status.getUser)
-        case s => suddenize(s.getText)
-      }
+      onStatusPattern(status).foreach( _(status) )
     }
 
     override def onFollow(source: User, followedUser: User) = {
@@ -31,7 +34,7 @@ package com.nekogata.SuddenKiller.twitter {
       if ( !source.isProtected && source.getScreenName != "totsuzenshi_bot") followBack(source)
     }
 
-    private def suddenize(text: String) = Suddenizer.suddenize(text) match {
+    private def suddenize(s: Status) = Suddenizer.suddenize(s.getText) match {
       case Some(suddenized) if suddenized.size < 140 => tweet(suddenized)
       case _ => Unit
     }
@@ -51,8 +54,8 @@ package com.nekogata.SuddenKiller.twitter {
       case (e: Throwable) => log.warn(e)
     }
 
-    private def unfollow(user: User) = try {
-      twitter.destroyFriendship(user.getId)
+    private def unfollow(s:Status) = try {
+      twitter.destroyFriendship(s.getUser.getId)
     } catch {
       case (e: Throwable) => log.warn(e)
     }
